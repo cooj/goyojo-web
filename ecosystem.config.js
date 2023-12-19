38212232a996664ad52777fc3afd857bd18ceb4c
cooj <z8888788@163.com>
2023-12-19 10:26:53 +0800

    chore(other): 初始化代码仓库，第一次提交代码更新

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps: [
+        {
+            name: 'nuxt-pro', // 设置PM2进程的名称
+            port: '8004',   // 项目运行的端口
+            exec_mode: 'cluster',
+            instances: 'max',
+            script: './.output/server/index.mjs',
+            env: {
+                // 数据库
+                DATABASE_URL: 'mysql://root:root@localhost:3306/nuxt-pro',  // 更改成实际的数据库
+            },
+        },
+    ],
+}
